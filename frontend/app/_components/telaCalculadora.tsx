9091646b75408e59cd337845777b5984975d6492
5hoxo <chocoshoxo@gmail.com>
2024-08-20 10:08:54 +0900

    [#1] - estilisação

---
+++ frontend/app/_components/telaCalculadora.tsx
@@ -0,0 +1,11 @@
+
+export default function TelaCalculadora() {
+  const alterarValor = () => {
+    console.log("")
+  }
+  return (
+    <div className=" bg-gray-500 pt-6 rounded-lg">
+      <span>addaf</span>
+    </div>
+  );
+}
