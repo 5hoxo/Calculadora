38d0e2de950af0bc1351f3de30b51e4fa9bcf1f1
5hoxo <chocoshoxo@gmail.com>
2024-08-20 12:11:37 +0900

    [#1] - estilisação dos botoes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,94 @@
 "use client"
 
 import { Button } from "@mui/material";
-import Image from "next/image";
 import TelaCalculadora from "./_components/telaCalculadora";
+
+const botoesCalculadora = [
+  "(",
+  ")",
+  "%",
+  "CE",
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ".",
+  "=",
+  "+"
+];
+
+enum botoesCalculadora2 {
+  colcheteL,
+  colcheteR,
+  porcentagem,
+  apagar,
+  sete,
+  oito,
+  nove,
+  divisao,
+  quatro,
+  cinco,
+  seis,
+  multiplicacao,
+  um,
+  dois,
+  tres,
+  menos,
+  zero,
+  ponto,
+  igual,
+  mais,
+}
 
 export default function Home() {
 
   const alterarValor = () => {
     console.log("abc")
   }
-  const buttons = [];
-  for (let i = 0; i< 4; i++) {
-    buttons.push(<Button className ="m-1 text-black text-center bg-gray-400 px-4 py-2 m-2"variant="contained" onClick={alterarValor}>button {i + 1}</Button>);
-  }
+  let posicao = 0
 
-
+  
   const fileira = [];
   for (let iterador = 0; iterador< 5; iterador++) {
+    const buttons = [];
+    for (let i = 0; i< 4; i++) {
+      let estilo = ""
+      let css = {}
+      switch (posicao) {
+        case botoesCalculadora2.apagar:
+          estilo = "text-white text-center bg-red-400 px-4 py-2 m-2"
+          
+          break
+        case botoesCalculadora2.colcheteL:
+        case botoesCalculadora2.colcheteR:
+        case botoesCalculadora2.porcentagem:
+        case botoesCalculadora2.divisao:
+        case botoesCalculadora2.multiplicacao:
+        case botoesCalculadora2.mais:
+        case botoesCalculadora2.menos:
+          css = {border: "2px solid #2196F380"}
+          estilo = "text-[#2196F380] text-center bg-white px-4 py-2 m-2"
+          break
+        case botoesCalculadora2.igual:
+          estilo = " px-4 py-2 m-2"
+          break
+        default:
+          estilo = "text-black text-center bg-gray-400 px-4 py-2 m-2"
+          break
+          
+      }
+      buttons.push(<Button className={estilo} style={css} variant="contained" onClick={alterarValor}>{botoesCalculadora[posicao]}</Button>);
+      posicao++
+    }
     fileira.push(<div className="flex">{buttons}</div>)
   }
   return (
